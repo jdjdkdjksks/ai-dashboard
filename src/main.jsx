4cf8aa3618baf0d4271a0290dce668272e3c7309
Luca Krickhan <luca@MacBook-Air-von-Luca.local>
2026-06-15 11:50:11 +0200

    Trigger fresh Vercel build

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Build Refresh: 2026-06-12T15:36:00
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
